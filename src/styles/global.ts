20f757b7c692db7b739a69c5a7369d3c732456af
br-gabriel <contato.gabrielfeitosa@gmail.com>
2022-03-22 01:01:58 -0300

    global style updated

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -11,7 +11,7 @@ export const GlobalStyle = createGlobalStyle`
         --background: #f8f2f5;
         --shape: #ffffff;
 	}
-    
+
 	* {
 			margin: 0;
 			padding: 0;
@@ -26,6 +26,15 @@ export const GlobalStyle = createGlobalStyle`
         @media (max-width: 720px) {
             font-size: 87.5%; //14px
         }
+    }
+
+    body, input, textarea, button {
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+    }
+
+    h1, h2, h3, h4, h5, h6, strong {
+        font-weight: 600;
     }
 
 	body {
