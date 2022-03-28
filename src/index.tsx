98cdf271ddceda13fc685d4f8506fdba6d654c6f
br-gabriel <contato.gabrielfeitosa@gmail.com>
2022-03-28 15:00:44 -0300

    remoção de transações deexemplo

--- src/index.tsx
+++ src/index.tsx
@@ -10,24 +10,7 @@ createServer({
 
   seeds(server) {
     server.db.loadData({
-      transactions: [
-        {
-          id: 1,
-          title: "Freelance de website",
-          type: "deposit",
-          category: "Dev",
-          amount: 6000,
-          createdAt: new Date('2022-01-29 09:00:00'),
-        },
-        {
-          id: 2,
-          title: "Aluguel",
-          type: "withdraw",
-          category: "Casa",
-          amount: 1100,
-          createdAt: new Date('2022-02-10 12:30:00'),
-        }
-      ],
+      transactions: [],
     })
   },
 
