0706e1d56209e4f06e137743360877cd830a25ce
br-gabriel <contato.gabrielfeitosa@gmail.com>
2022-11-10 00:51:26 -0300

    api url update

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://636c90c04f9c61473c081b64--fascinating-pegasus-e01941.netlify.app/api',
 })
